198463edc4d2f6f2a7da0eb4cf6b62ab0e42f10a
AZIZ BENMALLOUK <75162733+azar940@users.noreply.github.com>
2021-01-20 20:25:49 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace tictoctay
             int devesionPlayer = 1;
             char[] p = { '1','2','3','4','5','6','7','8','9' };
             bool weHaveWiner = false;
-
+            Int32 id = 0;
+            Random rnd = new Random();
+            
 
             do
             {
@@ -26,17 +28,30 @@ namespace tictoctay
                 }
                 else
                 {
+
+                    
                     Console.WriteLine(parx + parx + parx + "+");
                     Console.WriteLine("|       " + "Player : 2" + "      |");
                     Console.WriteLine(parx + parx + parx + "+");
                     playeronline = 'O';
+                    
                 }
                 
                 reprintGame(p, parx, parv);
-                Console.WriteLine("writing your choise :");
-                Int32 id = Convert.ToInt32(Console.ReadLine());
+                
+                if (playeronline == 'X')
+                {
+                    Console.WriteLine("writing your choise :");
+                    id = Convert.ToInt32(Console.ReadLine()) ;
+                    id--;
+                }
+                else
+                {
+                    id = Convert.ToInt32(rnd.Next(0, p.Length));
+                }
+                
 
-                p[id - 1] = playeronline;
+                p[id] = playeronline;
 
                 Console.Clear();
 
@@ -48,64 +63,38 @@ namespace tictoctay
             } while (! weHaveWiner);
             
         }
-
         static void reprintGame(char[] p,string parx,string parv) 
         {
             Console.WriteLine(parx + parx + parx + "+");
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine("|   " + p[0] + "   |   " + p[1] + "   |   " + p[2] + "   |");
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine(parx + parx + parx + "+");
 
-
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine("|   " + p[3] + "   |   " + p[4] + "   |   " + p[5] + "   |");
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine(parx + parx + parx + "+");
-
-
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine("|   " + p[6] + "   |   " + p[7] + "   |   " + p[8] + "   |");
-            Console.WriteLine(parv + parv + parv + "|");
-            Console.WriteLine(parx + parx + parx + "+");
-
+            for (int j=0;j<7;j+=3)
+            {
+                Console.WriteLine(parv + parv + parv + "|");
+                Console.WriteLine("|   " + p[0+j] + "   |   " + p[1 + j] + "   |   " + p[2 + j] + "   |");
+                Console.WriteLine(parv + parv + parv + "|");
+                Console.WriteLine(parx + parx + parx + "+");
+            }
         }
         static bool checkWiners(char[] p)
         {
             bool wHW = false;
             //player1
-            if (p[0] == 'X' && p[1] == 'X' && p[2] == 'X')
+            for (int j=0;j<7;j+=3)
             {
-                wHW = true;
-                printWiner("1");
+                if (p[j] == 'X' && p[j+1] == 'X' && p[j+2] == 'X')
+                {
+                    wHW = true;
+                    printWiner("1");
+                }
             }
-            if (p[3] == 'X' && p[4] == 'X' && p[5] == 'X')
+            for (int j=0;j<3;j+=1)
             {
-                wHW = true;
-                printWiner("1");
+                if (p[j] == 'X' && p[j + 3] == 'X' && p[j + 6] == 'X')
+                {
+                    wHW = true;
+                    printWiner("1");
+                }
             }
-            if (p[6] == 'X' && p[7] == 'X' && p[8] == 'X')
-            {
-                wHW = true;
-                printWiner("1");
-            }
-
-            if (p[0] == 'X' && p[3] == 'X' && p[6] == 'X')
-            {
-                wHW = true;
-                printWiner("1");
-            }
-            if (p[1] == 'X' && p[4] == 'X' && p[7] == 'X')
-            {
-                wHW = true;
-                printWiner("1");
-            }
-            if (p[2] == 'X' && p[5] == 'X' && p[8] == 'X')
-            {
-                wHW = true;
-                printWiner("1");
-            }
-
             if (p[0] == 'X' && p[4] == 'X' && p[8] == 'X')
             {
                 wHW = true;
@@ -117,41 +106,23 @@ namespace tictoctay
                 printWiner("1");
             }
 
-
             //player2
-
-            if (p[0] == 'O' && p[1] == 'O' && p[2] == 'O')
+            for (int j = 0; j < 7; j += 3)
             {
-                wHW = true;
-                printWiner("2");
+                if (p[j] == 'O' && p[j + 1] == 'O' && p[j + 2] == 'O')
+                {
+                    wHW = true;
+                    printWiner("1");
+                }
             }
-            if (p[3] == 'O' && p[4] == 'O' && p[5] == 'O')
+            for (int j = 0; j < 3; j += 1)
             {
-                wHW = true;
-                printWiner("2");
+                if (p[j] == 'O' && p[j + 3] == 'O' && p[j + 6] == 'O')
+                {
+                    wHW = true;
+                    printWiner("1");
+                }
             }
-            if (p[6] == 'O' && p[7] == 'O' && p[8] == 'O')
-            {
-                wHW = true;
-                printWiner("2");
-            }
-
-            if (p[0] == 'O' && p[3] == 'O' && p[6] == 'O')
-            {
-                wHW = true;
-                printWiner("2");
-            }
-            if (p[1] == 'O' && p[4] == 'O' && p[7] == 'O')
-            {
-                wHW = true;
-                printWiner("2");
-            }
-            if (p[2] == 'O' && p[5] == 'O' && p[8] == 'O')
-            {
-                wHW = true;
-                printWiner("2");
-            }
-
             if (p[0] == 'O' && p[4] == 'O' && p[8] == 'O')
             {
                 wHW = true;
@@ -163,8 +134,7 @@ namespace tictoctay
                 printWiner("2");
             }
 
-
-
+            //ta3adol
 
             if (p[0] != '1' && p[1] != '2' && p[2] != '3' && p[3] != '4' && p[4] != '5' && p[5] != '6' && p[6] != '7' && p[7] != '8' && p[8] != '9')
             {
@@ -172,7 +142,6 @@ namespace tictoctay
                 printWiner("0");
 
             }
-
 
             return wHW;
 
@@ -186,6 +155,5 @@ namespace tictoctay
             Console.WriteLine("+----------------------+");
                 
         }
-    
     }
 }
